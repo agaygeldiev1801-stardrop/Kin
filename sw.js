02432c3f454df48cd6bb24fe99717cc9847544f9
agaygeldiev1801-stardrop <agaygeldiev1801@icloud.com>
2026-03-26 19:50:52 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME='kino-v16-static';
+const URLS=['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(URLS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  event.respondWith(
+    caches.match(req).then(cached=>{
+      if(cached) return cached;
+      return fetch(req).then(res=>{
+        if(!res || res.status!==200 || req.url.startsWith('chrome-extension')) return res;
+        const copy=res.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(req,copy)).catch(()=>{});
+        return res;
+      }).catch(()=>caches.match('./index.html'));
+    })
+  );
+});
