8481d364023d9c29ebac4823ba9a306e13079ca9
agaygeldiev1801-stardrop <agaygeldiev1801@icloud.com>
2026-06-18 11:36:09 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-const CACHE='kino-v17';
+const C='kino-v16';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-  );
-});
-
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS).catch(()=>{})));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(
-    caches.match(e.request).then(hit=>{
-      if(hit)return hit;
-      return fetch(e.request).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-        return res;
-      }).catch(()=>caches.match('./index.html'));
-    })
-  );
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl)).catch(()=>{});return res;}).catch(()=>caches.match('./index.html'))));
 });
